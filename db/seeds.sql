9822cb69c196b30559bdc8f18275621ede26509e
page hunt <pagelhunt@gmail.com>
2020-05-30 20:31:23 -0600

    added file according to instructions and nearly completed project i think

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE eatDaBurger;
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("All am-er-rican", false),
+    ("Ez pz double beef baby Burger", false),
+    ("YOU GOT THE Burger", true),
+    ("Shack Burger", false);
+
+SELECT *
+FROM burgers
